a8e533550292ab0549b4589c5cd5a58fe4c87825
wachira <wachiraa954@gmail.com>
2022-07-15 15:24:19 +0300

    commit tree 176fe96e2d9d133a7135cbddd7f8cbc30cfb6919

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,18 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style= {{color: 'firebrick' }}>
+        {name}is a Web Developer from {city}
+        </h1>
+        <name />
+        <city />
+
+
+    </div>
+    );
+  
 }
 
 export default Home;
